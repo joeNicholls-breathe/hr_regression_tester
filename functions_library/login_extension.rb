03a175b8d9e6c1c3a383d3e86a42e0232f7b4501
Henzo67 <henry.cole@breathehr.com>
2023-02-23 16:47:37 +0000

    update 23-02-2023

--- functions_library/login_extension.rb
+++ functions_library/login_extension.rb
@@ -13,7 +13,7 @@ class LoginExtension
         def login_pass
                 driver.find_element(id: 'email-input').send_keys settings[:staging][:test_direct_admin_email]
                 sleep 1
-                driver.find_element(name: 'password').send_keys "Password1"
+                driver.find_element(name: 'password').send_keys settings[:staging][:login_password]
                 sleep 1
                 driver.find_element(css: 'form button[type=submit]').click 
                 sleep 1
