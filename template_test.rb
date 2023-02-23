c02d2c8fd2f1e5b0f9779424de542a5fd6c4e5b2
Henzo67 <henry.cole@breathehr.com>
2023-02-23 17:02:59 +0000

    update to library and script test

--- template_test.rb
+++ template_test.rb
@@ -1,23 +1,24 @@
 require 'selenium-webdriver'
 require './functions_library/navigate_browser_extension.rb'
 require './functions_library/sign_up_extension.rb'
-
-
+require_relative '../settings.rb'
 
 class TestName
+    include Settings
+
     attr_accessor :driver
+
     def initialize 
         @driver = Selenium::WebDriver.for :browser
     end
 
     def run_test
         begin
-            ExtensionName.new(@driver).functionName 
-            
+            ExtensionName.new(@driver).functionName       
         rescue => exception
             puts "Test - Output Name - Pass #{exception}"
         end
-
+        
         driver.close
     end
 end
