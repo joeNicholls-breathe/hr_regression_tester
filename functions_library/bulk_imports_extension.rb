ce92ea5768bb9b73da94bc7195930ec82ed97fac
Henzo67 <henry.cole@breathehr.com>
2023-02-24 15:22:13 +0000

    add bulk upload extension, upload files and add script

--- functions_library/bulk_imports_extension.rb
+++ functions_library/bulk_imports_extension.rb
@@ -1,16 +1,75 @@
 require File.expand_path('../base.rb', __FILE__)
+require File.expand_path('../account_employee_setup/positive_imports', __FILE__)
 
-class Extension < Base
-  def bulk_upload__name
+class BulkImportExtension < Base
+  def navigate_to_bulk_upload
     driver.find_element(css:'#navbar-nav-dropdown > ul > li:nth-child(3)').click
     driver.find_element(css:'#navbar-nav-dropdown a:nth-child(4)').click
-    # #navbar-nav-dropdown > ul > li.nav-item.dropdown.show > div > a:nth-child(4)
     driver.find_element(css:'body > section.content.container > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > a').click
     driver.find_element(css:'body > section.content.container > div.float-right > a').click
+  end
+
+  def bulk_upload_employee_full
     driver.find_element(id:'employees_data_import_type').click
     drop = driver.find_element(id:'employees_data_import_type')
-    choose.select_by(value:'employee_basic')
-    driver.find_element(id:'employees_data_import_import').send_keys "document.xls"
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Import new people')
+    driver.find_element(id:'employees_data_import_import').send_keys "employee_full.xlsx"
     driver.find_element(id:'submit-import').click
   end
+    
+  def bulk_upload_employee_basic
+    driver.find_element(id:'employees_data_import_type').click
+    drop = driver.find_element(id:'employees_data_import_type')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Import new people (basic)')
+    driver.find_element(id:'employees_data_import_import').send_keys "employee_basic.xlsx"
+    driver.find_element(id:'submit-import').click
+  end
+
+  def bulk_upload_update_employee_info
+    driver.find_element(id:'employees_data_import_type').click
+    drop = driver.find_element(id:'employees_data_import_type')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Update existing people')
+    driver.find_element(id:'employees_data_import_import').send_keys ".xlsx"
+    driver.find_element(id:'submit-import').click
+  end
+
+  def bulk_upload_jobs
+    driver.find_element(id:'employees_data_import_type').click
+    drop = driver.find_element(id:'employees_data_import_type')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, '')
+    driver.find_element(id:'employees_data_import_import').send_keys "jobs_details.xlsx"
+    driver.find_element(id:'submit-import').click
+  end
+
+  def bulk_upload_salaries
+    driver.find_element(id:'employees_data_import_type').click
+    drop = driver.find_element(id:'employees_data_import_type')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Salaries')
+    driver.find_element(id:'employees_data_import_import').send_keys "salary.xlsx"
+    driver.find_element(id:'submit-import').click
+  end
+  
+  def bulk_upload_benefits
+    driver.find_element(id:'employees_data_import_type').click
+    drop = driver.find_element(id:'employees_data_import_type')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Benefits')
+    driver.find_element(id:'employees_data_import_import').send_keys "benefits.xlsx"
+    driver.find_element(id:'submit-import').click
+  end
+
+  def bulk_upload_additional_payments
+    driver.find_element(id:'employees_data_import_type').click
+    drop = driver.find_element(id:'employees_data_import_type')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Additional payments')
+    driver.find_element(id:'employees_data_import_import').send_keys "additional_payments.xlsx"
+    driver.find_element(id:'submit-import').click
+  end
+
 end
