ec4e5b3fb573aa5b1eba2c0cc613a86e95bf58ef
Henzo67 <henry.cole@breathehr.com>
2023-04-13 16:58:40 +0100

    check dashboard for new starters and update to create account test script

---
+++ functions_library/test_page_check.rb
@@ -0,0 +1,10 @@
+require File.expand_path('../base.rb', __FILE__)
+
+class TestPageCheck
+  def check_pending_starters
+    searchtextbox = driver.find_element(xpath: '//*[@id="tab-hr-dashboard"]/div[2]/div[2]/div[3]/div[2]/div[2]').click
+    var AccountName = searchtextbox.getattribute('innerHTML')
+    string s = new.string(AccountName)
+    string s1 = new.string('Starting on 14/04/2023')
+  end
+end 
--- settings.rb
+++ settings.rb
@@ -5,13 +5,11 @@ module Settings
         name: 'UK Regression',
         breathe_url_login: 'https://login.breathehrstaging.com',
         breathe_url_signup: 'https://hr.breathehrstaging.com/signup',
-        test_sign_up_email: 'admin1011@regressionaccount.com',
-        test_direct_admin_email: 'admin01@regressionaccount.com',
+        sign_up_email: 'admin@regressionaccount.com',
         login_password: 'Password1',
         admin_email: 'admin@hr-regression.com',
-        line_manager_email: 'line@hr-regression-101.com',
-        employee_email: 'employee@ocun.com',
-        employee_e:'employee@hr-regression-101.com'
+        line_manager_email: 'line@hr-regression.com',
+        employee_email: 'employee@hr-regression.com'
       }
     }
   end
