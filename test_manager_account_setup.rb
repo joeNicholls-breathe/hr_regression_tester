108dc25ca70caca550ca8a9973d277a024813777
Henzo67 <henry.cole@breathehr.com>
2023-04-21 13:18:22 +0100

    account setup - settings extension

--- test_manager_account_setup.rb
+++ test_manager_account_setup.rb
@@ -1,8 +1,10 @@
 require 'selenium-webdriver'
-require './functions_library/navigate_browser_extension.rb'
-require './functions_library/navigate_around_app_manager.rb'
 require './functions_library/test_page_check.rb'
 require './functions_library/test_reference_extension.rb'
+require './functions_library/login_extension.rb'
+require './functions_library/login_app_extension.rb'
+require './functions_library/navigate_browser_extension.rb'
+require './functions_library/navigate_around_app_manager.rb'
 
 class AccountSetup
   attr_accessor :driver
@@ -13,16 +15,32 @@ class AccountSetup
   end
 
   def test_account_setup
-    login
-    navigate to configuration settings
-    enter company details
-    switch on modules - chargable
-    switch on modules - free
-    add a custom field
-    switch on payroll exports
-    switch on change logs
-    add onboarding tasks for employee
-    switch on xero integration *might want to do this as the buy now journey*
+    LoginExtension.login_admin
+    LoginAppExtension.select_hr
+    puts "login"
+    AppNavigationExtensionManager.navigate_to_settings
+    puts "navigate to configuration settings"
+    AccountConfigExtension.navigate_to_company_details
+    AccountConfigExtension.company_details_data_entry_edit
+    AccountConfigExtension.navigate_back_to_settings_breadcrumb
+    puts "enter company details"
+    AccountConfigExtension.navigate_to_modules_chargable
+    AccountConfigExtension.modules_chargable_switch_on
+    puts "switch on modules - chargable"
+    AccountConfigExtension.navigate_to_modules_free
+    AccountConfigExtension.modules_free_switch_on
+    puts "switch on modules - free"
+    AccountConfigExtension.custom_fields
+    puts "add a custom field"
+    AccountConfigExtension.payroll_export_log_changes_only
+    puts "switch on payroll exports"
+    AccountConfigExtension.person_change_logs
+    puts "switch on change logs"
+    AccountConfigExtension.onboarding_tasks_on
+    puts "add onboarding tasks for employee"
+    AccountConfigExtension.xero_payroll_integration
+    puts "switch on xero integration *might want to do this as the buy now journey"
+
     navigate to picklists
     details
     absence
