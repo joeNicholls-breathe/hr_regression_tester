f1ec67652f21e20f128f38a2994fc3f5cbcc0c5d
Henzo67 <henry.cole@breathehr.com>
2023-05-04 15:54:22 +0100

    further methods for account setup extension

--- functions_library/account_config_extension.rb
+++ functions_library/account_config_extension.rb
@@ -1,10 +1,24 @@
 require File.expand_path('../base.rb', __FILE__)
 
 class AccountConfigExtension < Base
+#Navigation
   def navigate_to_company_details
     driver.find_element(css: 'href="/account"').click
   end
 
+  def navigate_back_to_settings_breadcrumb
+    driver.find_element(css: 'href="/account/settings"').click
+  end
+
+  def navigate_to_modules_chargable
+    driver.find_element(css: 'href="/account/modules"').click
+  end
+
+  def navigate_to_picklist
+  driver find_element(css: 'href="/account/picklists"').click
+  end
+
+#Account Details data
   def company_details_data_entry_edit
     driver.find_element(class: 'href="/account/edit"').click
     #company address
@@ -48,15 +62,8 @@ class AccountConfigExtension < Base
     #driver.find_element(class: 'btn btn-outline-secondary').click
   end
 
-  def navigate_back_to_settings_breadcrumb
-    driver.find_element(css: 'href="/account/settings"').click
-  end
-
-  def navigate_to_modules_chargable
-    driver.find_element(css: 'href="/account/modules"').click
-  end
-
-  def modules_chargable_switch_on
+#Account Modules 
+   def modules_chargable_switch_on
     #Expenses
     driver.find_element(id: 'account_use_expenses_1').click
     #Recruitment - std
@@ -122,10 +129,8 @@ class AccountConfigExtension < Base
     driver.find_element(css: 'data-disable-with="update modules"').click
   end
 
-  def navigate_to_picklist
-  driver find_element(css: 'href="/account/picklists"').click
-  end
-
+#account Picklist configuration
+#contract type 
   def details_contract_types_add
     driver.find_element(css: 'href="/company_contract_types"').click
     driver.find_element(css: 'href="/company_contract_types/new"').click
@@ -174,6 +179,7 @@ class AccountConfigExtension < Base
     driver.find_element(class: 'btn btn-danger modal-confirm').click
   end
 
+#equipment type
   def details_equipment_types_add
     driver.find_element(css: 'href="/company_asset_types"').click
     driver.find_element(xpath: '/html/body/section[2]/div[2]/a/span/svg[2]').click
@@ -199,6 +205,7 @@ class AccountConfigExtension < Base
     driver.find_element(css: 'href="/account/picklists"').click
   end 
 
+#ethnicities
   def details_ethnicities_add
     driver.find_element(css: 'href="/company_ethnicities"').click
     driver.find_element(xpath: '/html/body/section[2]/div[2]/a/span').click
@@ -231,6 +238,7 @@ class AccountConfigExtension < Base
     driver.find_element(css: '#edit_gender_5833 > p > input').click
   end
 
+#gender
   def details_genders_edit
     driver.find_element(css: 'href="/genders"').click
     driver.find_element(css: '#DataTables_Table_0 > tbody > tr.odd > td.actions > a').click
@@ -243,41 +251,213 @@ class AccountConfigExtension < Base
     driver.find_element(css: 'href="/account/picklists"').click  
   end
   
+#id documents
+  def details_id_documents_types_add
+    driver.find_element(css: 'href="/company_identification_types"').click
+    driver.find_element(xpath: '/html/body/section[2]/div[2]/a/span/svg[1]').click
+    driver.find_element(id: 'company_identification_type_name').sendkeys "Regression test Add TODAY DATE"
+    driver.find_element(xpath: '//*[@id="new_company_identification_type"]/p/input').click
+  end
 
+  def details_id_documents_types_edit
+    driver.find_element(css: 'href="/company_identification_types"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0"]/tbody/tr[1]/td[2]').click
+    driver.find_element(id: 'company_identification_type_name').sendkeys "Regression test Edit TODAY DATE"
+    driver.find_element(xpath: '//*[@id="new_company_identification_type"]/p/input').click
+  end
 
+  def details_id_documents_types_delete
+    driver.find_element(css: 'href="/company_identification_types"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0"]/tbody/tr[6]/td[2]').click
+    driver.find_element(id: 'company_identification_type_name').sendkeys "Regression test TODAY DATE"
+    driver.find_element(xpath: '//*[@id="new_company_identification_type"]/p/input').click
+  end
 
-  def details_id_documents_types
+  def details_id_documents_types_search
+    driver.find_element(css: 'href="/company_identification_types"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0_filter"]/label/input').sendkeys "Regression test TODAY DATE"
+    driver.find_element(xpath: '//*[@id="new_company_identification_type"]/p/input').click
+  end
+
+  def details_id_documents_types_breadcrumb
+    driver.find_element(css: 'href="/company_identification_types"').click
+    driver.find_element(xpath: '/html/body/section[2]/div[2]/a/span/svg[1]').click
+    driver.find_element(css: 'href="/company_identification_types"').click
+  end
+
+#kudos types
+  def details_kudos_types_add
+    driver.find_element(css: 'href="/company_kudos_types"').click
+    driver.find_element(css:'href="/company_kudos_types/new"').click
+    driver.find_element(id:'company_kudos_type_name').sendkeys "Regression test add TODAY DATE"
+    driver.find_element(xpath:'//*[@id="new_company_kudos_type"]/p/input').click
+  end
+
+  def details_kudos_types_edit
+    driver.find_element(css: 'href="/company_kudos_types"').click
+    driver.find_element(:'//*[@id="DataTables_Table_0"]/tbody/tr[1]/td[3]/a[1]').click
+    driver.find_element(id:'company_kudos_type_name').sendkeys "Regression test edit TODAY DATE"
+    driver.find_element(xpath:'//*[@id="new_company_kudos_type"]/p/input').click
+  end
+
+  def details_kudos_types_delete
+    driver.find_element(css: 'href="/company_kudos_types"').click
+    driver.find_element(xpath:'//*[@id="DataTables_Table_0"]/tbody/tr[3]/td[3]/svg').click
+    driver.find_element(xpath:'//*[@id="delete_company_kudos_type_14155"]/div/div/div[3]/button[2]').click
+  end
+
+  def details_kudos_types_search
+    driver.find_element(css: 'href="/company_kudos_types"').click
+    driver.find_element(:'//*[@id="DataTables_Table_0_filter"]/label/input').sendkeys "Awesome service"
+  end
+
+  def details_kudos_types_breadcrumb
+    driver.find_element(css: 'href="/company_kudos_types"').click
+    driver.find_element(css:'href="/company_kudos_types/new"').click
+    driver.find_element(css: 'href="/company_kudos_types"').click
+  end
+
+#notice period
+  def details_notice_periods_add
+    driver.find_element(css: 'href="/company_noticeperiods"').click
+    driver.find_element(css:'href="/company_noticeperiods/new"').click
+    driver.find_element(id:'company_noticeperiod_name').sendkeys "Regression Add TODAY DATE"
+    driver.find_element(xpath:'//*[@id="new_company_noticeperiod"]/p/input').click
+  end
+
+  def details_notice_periods_edit
+    driver.find_element(css: 'href="/company_noticeperiods"').click
+    driver.find_element(css:'href="/company_noticeperiods/28331/edit"').click
+    driver.find_element(id:'company_noticeperiod_name').sendkeys "Regression Edit TODAY DATE"
+    driver.find_element(xpath:'//*[@id="new_company_noticeperiod"]/p/input').click
+  end
+
+  def details_notice_periods_delete
+    driver.find_element(css: 'href="/company_noticeperiods"').click
+    driver.find_element(xpath:'//*[@id="DataTables_Table_0"]/tbody/tr[2]/td[2]/svg').click
+    driver.find_element(:'').
+  end
+
+  def details_notice_periods_search
+    driver.find_element(css: 'href="/company_noticeperiods"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0_filter"]/label/input').sendkeys "1"
+    sleep 1
+  end
+
+  def details_notice_periods_breadcrumb
+    driver.find_element(css: 'href="/company_noticeperiods"').click
+    driver.find_element(css:'href="/company_noticeperiods/new"').click
+    driver.find_element(xpath:'//*[@id="delete_noticeperiod_28337"]/div/div/div[3]/button[2]').click
+  end
+
+#pronouns
+  def details_pronouns_add
+    driver.find_element(css: 'href="/company_pronouns"').click
+    driver.find_element(css: 'href="/company_pronouns/new"').click
+    driver.find_element(id: 'company_pronoun_name').sendkeys "Regression Add TODAY DATE"
+    driver.find_element(xpath: '//*[@id="new_company_pronoun"]/p/input').click
+  end
+
+  def details_pronouns_cancel_add
+    driver.find_element(css: 'href="/company_pronouns"').click
+    driver.find_element(css: 'href="/company_pronouns/new"').click
+    driver.find_element(css: 'href="/company_pronouns"').click
+  end
+
+  def details_pronouns_edit
+    driver.find_element(css: 'href="/company_pronouns"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0"]/tbody/tr[1]/td[2]').click
+    driver.find_element(id: 'company_pronoun_name').sendkeys "Regression Add TODAY DATE"
+    driver.find_element(xpath: '//*[@id="new_company_pronoun"]/p/input').click
+  end
+
+  def details_pronouns_delete
+    driver.find_element(css: 'href="/company_pronouns"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0"]/tbody/tr[2]/td[2]/svg').click
     driver.find_element(: '').
   end
 
-  def details_kudos_types
-    driver.find_element(: '').
+  def details_pronouns_search
+    driver.find_element(css: 'href="/company_pronouns"').click
+    driver.find_element(xpath: '//*[@id="DataTables_Table_0_filter"]/label/input').sendkeys "Him"
+    sleep 1
   end
 
-  def details_notice_periods
-    driver.find_element(: '').
+  def details_pronouns_breadcrumb
+    driver.find_element(css: 'href="/company_pronouns"').click
+    driver.find_element(css: 'href="/company_pronouns/new"').click
+    driver.find_element(css: 'href="/company_pronouns"').click
   end
 
-  def details_pronouns
-    driver.find_element(: '').
-  end
-
+#reason for leaving
   def details_reason_for_leaving
-    driver.find_element(: ''). 
+    driver.find_element(css: 'href="/company_reason_for_leaving_types"').click
   end
 
+#onboarding tasks
   def details_onboarding_tasks
-    driver.find_element(: '').
+    driver.find_element(css: 'href="/company_tasks"').click
   end
 
-
-
-
-
-  def absence
+#other leave types
+  def absence_other_leave_reasons
+    driver.find_element(css: 'href="/company_leave_reasons"').click
   end
 
+#sickness types
+  def absence_sickness_types
+    driver.find_element(css: 'href="/company_sicknesstypes"').click
+  end
 
+#training categories
+  def training_categories
+    driver.find_element(css: 'href="/company_training_categories"').click
+  end 
 
+#training provider
+  def training_providers
+    driver.find_element(css: 'href="/company_training_providers"').click
+  end 
+
+#training types
+  def training_types
+    driver.find_element(css: 'href="/company_training_types"').click
+  end 
+
+#departments
+  def company_organisation_department
+    driver.find_element(css: 'href="/company_departments"').click  
+  end
+
+#divisions
+  def company_organisation_division
+    driver.find_element(css: 'href="/company_divisions"').click
+  end
+
+#additional payment types
+  def pay_and_benefits_additional_payment_types
+    driver.find_element(css: 'href="/company_additional_payment_types"').click
+  end
+
+#benefit types
+  def pay_and_benefits_benefit_types
+    driver.find_element(css: 'href="/company_benefit_types"').click
+  end
+ 
+#disciplinary outcomes
+  def performance_management_disciplinary_outcomes
+    driver.find_element(css: 'href="/company_disciplinary_outcomes"').click
+  end
+  
+#grievence outcomes
+  def performance_management_grievance_outcomes
+    driver.find_element(css: 'href="/company_grievance_outcomes"').click
+  end
+  
+#one to ones
+  def performance_management_one_to_one_types
+    driver.find_element(css: 'href="/company_one_to_one_types"').click
+  end
+  
 
 end
