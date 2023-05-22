f63160f5ad17dfb51c466ec1a3383c519b24242b
Henzo67 <henry.cole@breathehr.com>
2023-05-22 16:51:32 +0100

    added new file extension for config reporting and added working pattern methods

--- settings.rb
+++ settings.rb
@@ -9,7 +9,8 @@ module Settings
         login_password: 'Password1',
         admin_email: 'admin@hr-regression.com',
         line_manager_email: 'line@hr-regression.com',
-        employee_email: 'employee@hr-regression.com'
+        employee_email: 'employee@hr-regression.com',
+        setup_acc_email: 'admin@auto-reg.co.uk'
       }
     }
   end
