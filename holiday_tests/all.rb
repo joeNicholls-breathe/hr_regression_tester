d6b450fd9b3db0dbfe728b09c1fde99c34cbabf7
Michael Redgrave <michaelredgrave@BREATHE-LT114.local>
2023-10-18 12:24:30 +0100

    Add carry over absence test
    Why?
    Add new test to check carry over is being used correctly when creating absence records
    
    How?
    As Admin user
    - creates new record for the last absence year, to create carry over for current year
    - Add new record in carry over period
    - check totals

--- holiday_tests/all.rb
+++ holiday_tests/all.rb
@@ -3,3 +3,5 @@
 require './holiday_tests/test_employee_holiday_process'
 require './holiday_tests/test_employee_holiday_totals_on_non_working_days'
 require './holiday_tests/test_employee_holiday_two_years'
+require './holiday_tests/test_employee_carry_over'
+require './holiday_tests/test_employee_leave_sickness_clash'
