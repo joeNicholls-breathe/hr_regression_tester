41a0247df444f599a0b5cdc04988b6ac03388522
Michael Redgrave <michaelredgrave@BREATHE-LT114.local>
2023-12-13 16:37:57 +0000

    BTE-6432: Automation testing sprint 58
    Create test to check other leave does not deduct from allowance

---
+++ functions_library/other_leave_request_extension.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require File.expand_path('base.rb', __dir__)
+
+class OtherLeaveRequestExtension < Base
+  # rubocop:disable Metrics/AbcSize
+  def employee_holiday_other_leave_request
+    driver.find_element(id: 'leave_request_kind_of_otherleave').click # other
+    drop = driver.find_element(id: 'leave_request_company_leave_reason_id')
+    choose = Selenium::WebDriver::Support::Select.new(drop)
+    choose.select_by(:text, 'Compassionate')
+    driver.find_element(css: '#\#leave_request_start_date_react').send_keys '15/12/2023' # start date
+    driver.find_element(css: '#\#leave_request_end_date_react').send_keys '15/12/2023' # end date
+    driver.find_element(id: 'leave_request_notes').send_keys 'Test other leave request' # note
+    driver.find_element(xpath: '//*[@id="new_leave_request"]/p/input').click
+  end
+  # rubocop:enable Metrics/AbcSize
+end
