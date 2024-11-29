8014d8970db0ede02b9b0621c02f157115d3764e
Henzo67 <henry.cole@breathehr.com>
2024-11-29 17:24:16 +0000

    updates to RTA scripts in order to use Rspec and the underlying methods

--- user_functionality/rota/test_rota_chrome.rb
+++ user_functionality/rota/test_rota_chrome.rb
@@ -3,99 +3,183 @@
 require 'selenium-webdriver'
 require 'logger'
 require 'rspec'
+require 'pry'
 require './functions_library/ui_page_element_check'
 require './functions_library/test_reference_extension'
 require './functions_library/login_extension'
 require './functions_library/login_app_extension'
 require './functions_library/navigate_browser_extension'
+require './functions_library/rota_extension'
 require './settings'
-
 # rubocop:disable Metrics/BlockLength
 
 RSpec.describe 'Rota Regression test script' do
   before do
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
     @driver = Selenium::WebDriver.for :chrome, options:
   end
 
   # accessing Rota application
-
-  it 'navigate to breathe user login' do
+  it '1a. navigate to breathe user login' do
     NavigateBrowserExtension.new(@driver).breathe_login
     expect(@driver.title).to eql('Log in to Breathe')
   end
 
-  it 'login as rota admin user via RTA widget' do
+  it '1b. login as rota admin user via RTA widget' do
     NavigateBrowserExtension.new(@driver).breathe_login
     LoginExtension.new(@driver).login_rota_admin
+    sleep 0.25
     LoginAppExtension.new(@driver).select_rota
     sleep 0.25
     expect(@driver.title).to eql('Rota, Time & Attendance')
   end
 
-  it 'navigate to rota from hr as admin user' do
+  it '1c. navigate to rota from hr as admin user' do
     NavigateBrowserExtension.new(@driver).breathe_login
     LoginExtension.new(@driver).login_rota_admin
+    sleep 0.25
     LoginAppExtension.new(@driver).select_hr
     sleep 0.25
+    RotaExtension.new(@driver).navigate_to_rota_from_hr_admin
     expect(@driver.title).to eql('Rota, Time & Attendance')
   end
 
-  it 'navigate to timesheets from hr as admin user' do
+  it '1d. navigate to timesheets from hr as admin user' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_admin
+    sleep 0.25
+    LoginAppExtension.new(@driver).select_hr
+    sleep 0.25
+    RotaExtension.new(@driver).navigate_to_timeandattendance_daily_from_hr_admin
+    sleep 2
+    expect(@driver.title).to eql('Timesheets')
+  end
+
+  it '1e. navigate to rota as employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_rota
+    sleep 1
+    expect(@driver.title).to eql('Rota, Time & Attendance')
+  end
+
+  it '1f. navigate to rota as employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_hr
+    sleep 0.25
+    RotaExtension.new(@driver).my_employee_dashboard_rota
+    sleep 1
+    expect(@driver.title).to eql('Employee dashboard | My Rosters')
+  end
+
+  it '1g. next 7 days of rota as employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_hr
+    sleep 0.25
+    RotaExtension.new(@driver).my_employee_dashboard_rota
+    sleep 0.25
+    RotaExtension.new(@driver).employee_view_next_seven_days
+    sleep 1
+    RotaExtension.new(@driver).employee_check_assigned_rota_template
+    expect(@driver.title).to eql('Employee dashboard | My Rosters')
+  end
+
+  # shifts create/edit/cancel/delete/switch to an employee
+  it '2a. assign and share shift to an employee' do
     NavigateBrowserExtension.new(@driver).breathe_login
     LoginExtension.new(@driver).login_rota_admin
     LoginAppExtension.new(@driver).select_rota
     sleep 0.25
-    expect(@driver.title).to eql('Rota, Time & Attendance')
-  end
-
-  # shifts create/assign/edit/cancel/delete/switch to an employee
-
-  it 'assign and share shift to an employee' do
-    NavigateBrowserExtension.new(@driver).breathe_login
-    LoginExtension.new(@driver).login_rota_admin
-    LoginAppExtension.new(@driver).select_rota
-    sleep 0.25
-    RotaExtension.new(@driver).assign_shift
+    RotaExtension.new(@driver).assign_one_shift_monday_std_employee
     sleep 1
     RotaExtension.new(@driver).share_shift
+    sleep 2
+    employee_monday_shift = @driver.find_element(class: 'roster-map-1-0')
+    monday_shift_time_element = employee_monday_shift.find_element(class: 'shift-card-view__time')
+    expect(monday_shift_time_element.attribute('innerHTML')).to eql('10:12-17:13')
+  end
+
+  it '2b. edit shift to an employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_admin
+    LoginAppExtension.new(@driver).select_rota
+    sleep 0.25
+    RotaExtension.new(@driver).edit_shift
     sleep 1
-    expect(@driver.title).to eql('')
+    RotaExtension.new(@driver).share_shift
+    sleep 2
+    employee_monday_shift = @driver.find_element(class: 'roster-map-1-0')
+    monday_shift_time_element = employee_monday_shift.find_element(class: 'shift-card-view__time')
+    expect(monday_shift_time_element.attribute('innerHTML')).to eql('08:00-16:30')
+  end
+
+  it '2c. delete shift that was assigned to the employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_rota
+    sleep 0.25
+    RotaExtension.new(@driver).delete_shift
+  end
+
+  it '2d. rota employee notification' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    sleep 0.25
+    LoginAppExtension.new(@driver).select_rota
+    sleep 0.25
+    RotaExtension.new(@driver).employee_view_next_seven_days
+    sleep 0.25
+    RotaExtension.new(@driver).view_notifications_employee_shifts
+    expect(@driver.title).to eql('Employee dashboard | My Rosters')
+  end
+
+  it '2e. decline shift assigned to the employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_rota
+    sleep 0.25
+    RotaExtension.new(@driver).employee_view_next_seven_days
+    RotaExtension.new(@driver).decline_shift
+  end
+
+  it '2f. swap shift assigned to the employee' do
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_rota
+    sleep 0.25
+    RotaExtension.new(@driver).employee_view_next_seven_days
+    RotaExtension.new(@driver).swap_shift
   end
 
   # templates create/assign/edit/cancel/delete/switch to an employee
-
-  it 'create a template for an employee' do
+  it '3a. create a template for an employee' do
     NavigateBrowserExtension.new(@driver).breathe_login
     LoginExtension.new(@driver).login_rota_admin
     LoginAppExtension.new(@driver).select_rota
     sleep 0.25
     RotaExtension.new(@driver).create_template
-    expect(@driver.title).to eql('')
+    expect(@driver.title).to eql('Rota week view - user view')
   end
 
-  it 'delete a template for an employee' do
+  it '3b. delete a template for an employee' do
     NavigateBrowserExtension.new(@driver).breathe_login
     LoginExtension.new(@driver).login_rota_admin
     LoginAppExtension.new(@driver).select_rota
     sleep 0.25
     RotaExtension.new(@driver).delete_template
-    expect(@driver.title).to eql('')
+    expect(@driver.title).to eql('Rota week view - user view')
   end
 
-  it 'assign a template for an employee' do
+  it '3c. assign a template for an employee' do
     NavigateBrowserExtension.new(@driver).breathe_login
     LoginExtension.new(@driver).login_rota_admin
     LoginAppExtension.new(@driver).select_rota
-    expect(@driver.title).to eql('')
+    expect(@driver.title).to eql('Rota week view - user view')
   end
-
-  # manage employees
-  # reports
-  # employee managing themselves
-
   # rubocop:enable Metrics/BlockLength
 end
---
+++ user_functionality/rota/test_rota_practice.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'selenium-webdriver'
+require 'logger'
+require './functions_library/ui_page_element_check'
+require './functions_library/test_reference_extension'
+require './functions_library/login_extension'
+require './functions_library/login_app_extension'
+require './functions_library/logout_extension'
+require './functions_library/navigate_browser_extension'
+require './functions_library/rota_extension'
+require './settings'
+
+class Rota < Base
+  attr_accessor :driver
+
+  def initialize
+    @driver = Selenium::WebDriver.for :chrome
+    Selenium::WebDriver.logger.level = :info
+  end
+
+  def test_rota
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_admin
+    LoginAppExtension.new(@driver).select_rota
+    RotaExtension.new(@driver).assign_one_shift_monday_std_employee
+    RotaExtension.new(@driver).share_shift
+    # HAVING ISSUES AMENDING THE DATES OF A SHIFT AS ONCE YOU CLEAR THE CELL/FIELD THE ORIGINAL
+    # NUMBER FOR START AND FINISH TIME SOMEHOW IS STILL CACHED IN THE COMMAND
+    # RotaExtension.new(@driver).edit_shift_monday_std_employee
+    # RotaExtension.new(@driver).share_shift
+    RotaExtension.new(@driver).delete_shift
+    RotaExtension.new(@driver).assign_one_shift_monday_std_employee
+    RotaExtension.new(@driver).share_shift
+    RotaExtension.new(@driver).assign_multiple_shifts_std_employee
+    sleep 1
+    RotaExtension.new(@driver).share_shift
+    # ON THE SAVE OF THE TEMPLATE WE HAVE AN ISSUE THAT IT DOESNT SAVE FROM THE AUTOMATION ALTHOUGH 
+    # IS FINE FROM MANUAL SAME ISSUE ABOVE WITH MANUAL CHANGE WORKS FINE
+    RotaExtension.new(@driver).create_template
+    RotaExtension.new(@driver).assign_template
+    RotaExtension.new(@driver).share_shift
+    # DELETE WONT REMOVE THE PREVIOUS TEMPLATE FOR SOME REASON
+    RotaExtension.new(@driver).delete_template
+    LogoutExtension.new(driver).logout_admin
+    # employee
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_employee
+    LoginAppExtension.new(@driver).select_hr
+    RotaExtension.new(@driver).my_employee_dashboard_rota
+    RotaExtension.new(@driver).employee_view_next_seven_days
+    #CURRENT POSITION
+    RotaExtension.new(@driver).employee_check_assigned_rota_template # need to make sure template is with 30 days or today
+    RotaExtension.new(@driver).view_notifications_employee_shifts
+    RotaExtension.new(@driver).decline_shift
+    RotaExtension.new(@driver).swap_shift
+    LogoutExtension.new(driver).user_logout
+  end
+
+  def timesheets
+    NavigateBrowserExtension.new(@driver).breathe_login
+    LoginExtension.new(@driver).login_rota_admin
+    LoginAppExtension.new(@driver).select_hr
+    RotaExtension.new(@driver).navigate_to_timeandattendance_daily_from_hr_admin
+  end
+end
+
+Rota.new.test_rota
