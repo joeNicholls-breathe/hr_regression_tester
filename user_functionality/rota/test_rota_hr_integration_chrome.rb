970ca275918ed85aa64da892976282995f75b820
Henzo67 <henry.cole@breathehr.com>
2025-01-29 19:30:07 +0800

    integration test cases for RTA with info coming from HR, Why? for holiday and sickness testing

---
+++ user_functionality/rota/test_rota_hr_integration_chrome.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'selenium-webdriver'
+require 'logger'
+require 'rspec'
+require 'pry'
+require './functions_library/ui_page_element_check_extension'
+require './functions_library/test_reference_extension'
+require './functions_library/login_extension'
+require './functions_library/login_app_extension'
+require './functions_library/navigate_browser_extension'
+require './functions_library/rota_manager_extension'
+require './functions_library/rota_employee_extension'
+require './functions_library/timesheets_extension'
+require './functions_library/timesheets_employee_extension'
+require './settings'
+# rubocop:disable Metrics/BlockLength
+
+RSpec.describe 'Timesheet Regression test script' do
+  before do
+    @sleep_time_long = (ENV['SLEEPTIME_LONG'] || 4).to_f
+    @sleep_time_short = (ENV['SLEEPTIME_SMALL'] || 1).to_f
+    options = Selenium::WebDriver::Chrome::Options.new
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--window-size=1920,1080')
+    @driver = Selenium::WebDriver.for :chrome, options:
+  end
+
+  # accessing Timesheet application as users
+  it '1a. RTA admin - login and navigate to TIMESHEEET from hr' do
+  end
+  # rubocop:enable Metrics/BlockLength
+end
